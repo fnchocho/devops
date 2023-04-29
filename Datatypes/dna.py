03af730c808e664e43648cf8c35052bdb9ffa0f7
Fidelis Nchocho <fnchocho@gmail.com>
2023-04-29 12:02:45 +0000

    create dna.py

--- Datatypes/dna.py
+++ Datatypes/dna.py
@@ -1,1 +1,12 @@
+import random
 
+bases = ['A','T','C','G']
+strand1 = random.choices(bases,k=10)
+print(strand1)
+
+dna = {
+    key:
+    [val, ('T' if val == 'A' else 'A' if val == 'T' else 'C' if val == 'G' else 'G')] 
+    for (key, val) in enumerate(strand1)
+    }
+print(dna)
