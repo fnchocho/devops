09d0b760d93eabf46bc3b055c527b539bca0a71d
Fidelis Nchocho <fnchocho@gmail.com>
2023-05-02 14:30:20 +0000

    Update top_level.py

--- Functions/top_level.py
+++ Functions/top_level.py
@@ -1,1 +1,8 @@
+import math                 #Top level and entry point of current code
 
+df = math.factorial(3)      # not top level, borrowed, never specified in our console
+print("This is a module")   # top level 
+print(math.__name__)        # display the name of the top level env where top level code lives 
+
+
+print('============================================================\n')
