6370013eed64ee08e7018b7035cbfc31ac3e3f03
fnchocho <fnchocho@gmail.com>
2023-05-08 12:20:41 -0400

    Create days converter code

---
+++ Functions/days_converter.py
@@ -0,0 +1,12 @@
+
+num_days = 1
+hours_per_day = 24
+
+def num_hours(num_days, custom_message):
+    num_hours = num_days * hours_per_day
+    print(f"There are {num_hours} hours in {num_days}.")
+    print(f"{custom_message}!!") 
+
+num_hours(5, "Superb" )
+
+  
